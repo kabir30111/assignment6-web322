17c2f5c4d86ee6c1306b0f88a04b84907aa93e35
Shubham <shubham20@myseneca.ca>
2025-02-03 18:32:23 -0500

    Updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const projectData = require("./modules/projects");
 
 // Create an Express app
 const app = express();
-const PORT = process.env.PORT || 3000;  
+const PORT = process.env.PORT || 4000;  
 
 
 app.use(express.json());
